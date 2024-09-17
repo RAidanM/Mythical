886e7a176aed9d9f73094c504598eb67458b6f12
RAidanM <r.aidan.mealey@gmail.com~>
2024-09-17 16:58:59 -0400

    First Working Card Object

---
+++ card.js
@@ -0,0 +1,39 @@
+isGrabbed = false;
+grabbedCard = null;
+offsetX = 0;
+offsetY = 0;
+
+function grab(event, cardElement){
+    console.log("grabbed");
+    grabbedCard = cardElement;
+    isGrabbed = true;
+
+    const bounds = cardElement.getBoundingClientRect();
+    offsetX = event.clientX - bounds.left;
+    offsetY = event.clientY - bounds.top;
+
+    document.addEventListener("mousemove",move);
+    document.addEventListener("mouseup",drop);
+
+    event.preventDefault(); //avoids text selection
+}
+
+function move(event){
+    if(isGrabbed && grabbedCard){
+        grabbedCard.style.left = `${event.clientX - offsetX}px`;
+        grabbedCard.style.top = `${event.clientY - offsetY}px`;
+    }
+}
+
+function drop(){
+    isGrabbed = false;
+    grabbedCard = null;
+    document.removeEventListener("mousemove",move);
+    document.removeEventListener("mouseup",drop);
+}
+
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mousedown', function(event) {
+        grab(event, card);
+    });
+});
