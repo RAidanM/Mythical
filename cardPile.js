86b3cfecdc25806e210211c6a3a553caca542a80
RAidanM <r.aidan.mealey@gmail.com>
2024-09-19 16:26:26 -0400

    Added multiple piles

--- cardPile.js
+++ cardPile.js
@@ -5,12 +5,15 @@ function takeCard(event, pileElement){
     let newCard = document.createElement('div');
     newCard.classList.add("card");
     newCard.innerHTML = card_value++;
+    newCard.style.top = pileElement.style.top;
+    newCard.style.left = pileElement.style.left;
 
     document.body.appendChild(newCard);
 
     newCard.addEventListener('mousedown', function(event) {
         grab(event, newCard);
     });
+    grab(event, newCard);
 }
 
 document.querySelectorAll('.pile').forEach(pile => {
